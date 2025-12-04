1ceda8a8aa601f836fcd8fbef9dd81be0cc83757
Daksha-Belawadi <belawadidaksha@gmail.com>
2025-12-04 07:05:00 +0530

    save

---
+++ bmi.py
@@ -0,0 +1,25 @@
+import sys
+
+if len(sys.argv) == 3:
+    print("User provided values")
+    weight = sys.argv[1]
+    height = sys.argv[2]
+else:
+    print("No input given - using default values")
+    weight = 50
+    height = 1.5
+
+bmi = weight / (height * height)
+
+print("Weight:", weight)
+print("Height:", height)
+print("BMI:", bmi)
+
+if bmi < 18:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal")
+elif bmi < 30:
+    print("Overweight")
+else:
+    print("Obese")
